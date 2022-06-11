5d46659130d59d3a9242e1f4e1710a157f00d456
Deftones565 <fearfactory2006@hotmail.com>
2022-06-12 05:13:08 +0800

    changing UI to find path better

--- src/eXit/UI.py
+++ src/eXit/UI.py
@@ -1,6 +1,24 @@
 import climage
+import os
 
+class UI():
 
+    def __init__(self):
+        self.location = os.getcwd()
+        self.image = "\\images\\"
+        self.screen = "0"
+        self.png = ".png"
+        self.animationPath = "animation\\"
 
+    def changeScreen(self, count):
+        print(climage.convert(self.location+self.image+count+self.png, is_unicode=True, is_truecolor=False, is_256color=False, is_16color=True, is_8color=False, width=120, palette="default"))
 
-print(climage.convert("C:\\Users\\fearf\\program\\eXit\\images\\1.png", is_unicode=False, is_truecolor=True, is_256color=False, is_16color=False, is_8color=False, width=120, palette="default"))
+    # This is rough on the eyes
+#    def animation(self):
+#        count = 0
+#        while(1):
+#            os.system("cls")
+#            print(climage.convert(self.location+self.animationPath+str(count)+self.png, is_unicode=False, is_truecolor=True, is_256color=False, is_16color=False, is_8color=False, width=120, palette="default"))
+#            count += 1
+#            if(count >= 28):
+#                break
