2d6e01da4d574842473673a9be8999a15db45fae
Deftones565 <fearfactory2006@hotmail.com>
2022-06-12 04:49:47 +0800

    completed

---
+++ src/eXit/main.py
@@ -0,0 +1,75 @@
+import UI
+import msvcrt as m
+import os
+
+
+class main():
+
+    def __init__(self):
+
+        self.UI = UI.UI()
+
+        self.story = ["You're trapped in a dungeon with your friend.\nYou see a barrel. What do you do?",
+            "The barrel rolls aside and you find a secret tunnel. What do you do?",
+            "You start to escape but your friend is too weak to\ngo with you. They hand you a note.\nWhat do you do?",
+            "It is too dark to read the note.\nWhat do you do?",
+            "You crawl through the tunnel and the tunnel leads\nto a beach. What do you do?",
+            "In the water you see a boat.\nWhat do you do?",
+            "Congratulations, you're heading to a new world!\nDo you want to play again?",
+            "You're trapped in a dungeon with your friend... Is it\nthe same dungeon? You see a barrel. What do you do?",
+            "Your friend hands you a note.\nWhat do you do?",
+            "The note says, \"Don't leave me here.\"\nDo you leave your friend or stay?"]
+
+        self.answers = ["move the barrel",
+            "enter the tunnel",
+            "read the note",
+            "leave",
+            "look",
+            "get on the boat",
+            "yes",
+            "sit down next to my friend",
+            "light a match",
+            "stay"]
+
+        self.inncorrectCount = 0
+
+        self.inncorrect = ["You can't do that here.",
+            "You can't do that now",
+            "You could do that before",
+            "You could do that later",
+            "But it is now",
+            "And it will always be now",
+            "What is it, really?"]
+
+        self.count = 0
+
+    def anyKey(self):
+        print("Press any key to start")
+        m.getch()
+        os.system("cls")
+
+
+    def helloFriend(self):
+        self.UI.changeScreen(str(self.count))
+        self.anyKey()
+        self.count += 1
+        for s, a in zip(self.story, self.answers):
+            self.UI.changeScreen(str(self.count))
+            while(1):
+                print(s)
+                answer = input("> ")
+                if(answer == a):
+                    self.count += 1
+                    os.system("cls")
+                    break
+                else:
+                    os.system("cls")
+                    self.UI.changeScreen(str(self.count))
+                    print(self.inncorrect[self.inncorrectCount])
+                    self.inncorrectCount += 1
+                    if(self.inncorrectCount == 7):
+                        self.inncorrectCount = 0
+
+if(__name__ == "__main__"):
+    bonsoirElliot = main()
+    bonsoirElliot.helloFriend()
